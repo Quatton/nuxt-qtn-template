0ebaf7706a9e0dc13d7ac142485833f8f659d7af
Quatton <atomicatives@gmail.com>
2023-07-09 17:15:52 +0900

    feat: test nuxtui and tailwind prose

---
+++ components/layout/header.vue
@@ -0,0 +1,15 @@
+<template>
+  <header class="sticky top-0 z-40 border-b dark:border-gray-800">
+    <div class="container flex h-16 items-center px-4">
+      <div
+        class="font-bold text-2xl bg-gradient-to-br from-primary-500 to-primary-700 text-transparent bg-clip-text"
+      >
+        <NuxtLink to="/">Brand</NuxtLink>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script setup></script>
+
+<style></style>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="py-10 px-4 prose dark:prose-invert">
+    <h1>Quatton's Nuxt.js Template</h1>
+    <h2>Minimal But Opinionated</h2>
+    <ul>
+      <li>NuxtLabs UI (Tailwind CSS + Nuxt Colormode)</li>
+      <li>Use Nuxt's API route type safety instead of tRPC</li>
+      <li>ESLint with recommended settings</li>
+    </ul>
+  </section>
+</template>
+
+<script setup></script>
+
+<style></style>
